f60db7067e53400b685eaeeb91d68894177aef64
The-Secretive <XHJ1656426916@outlook.com>
2022-08-11 05:01:28 +0800

    更新伪随机

--- Assets/Scripts/GlobalDefine.cs
+++ Assets/Scripts/GlobalDefine.cs
@@ -161,13 +161,21 @@ public enum EffectType
     //*亡语*，*先机*，*每回合开始时*，*每回合结束时*, boss的特殊技能
 }
 
-public static class LevelEvent
+/// <summary>
+/// 游戏局内全局常量
+/// </summary>
+/// <returns></returns>
+public static class GameConst
 {
     public static int[] GameEventCount = { 0, 5, 6, 7, 8 };//初始化每层关卡数量（1-4）
     public static int[] level_1 = { 1, 1, 3, 3, 4 };
     public static int[] level_2 = { 1, 1, 3, 3, 4, 2 };
     public static int[] level_3 = { 1, 1, 1, 2, 3, 3, 5 };
     public static int[] level_4 = { 1, 1, 1, 3, 3, 4, 2, 5 };
+
+    public static int[] DrawCard = { 1,1,1,1,2,2,2,3,3,4 };
+    public static int[,] Card_SVN = { { 0, 0 }, { 1, 23 }, { 50, 78 }, { 100, 117 }, { 150, 159 } };
+    public static int[,] Card_SPL = { { 0, 0 }, { 201, 249 }, { 250, 299 }, { 300, 349 }, { 350, 399 } };
 
     public static int[] GetArrray(int[] _target)
     {
