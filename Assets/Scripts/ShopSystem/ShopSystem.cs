9df2e9fe5bb33cc142a5a991a5022fcced1e5784
TtrtiumQ <qun02112@126.com>
2022-08-17 15:45:04 +0800

    fix

---
+++ Assets/Scripts/ShopSystem/ShopSystem.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopSystem : MonoBehaviour
+{
+    public ShopType shopType;
+
+}
+public enum ShopType
+{
+    Void,
+    Shop,
+    ShopInGame,
+}
+
